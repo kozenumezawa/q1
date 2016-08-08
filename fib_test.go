eb2e0b7e16b23bf8a9b0ded767f7aef4dd601163
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-08-08 21:44:14 +0900

    Add 'fib_test.go' for a unit test

--- fib_test.go
+++ fib_test.go
@@ -4,10 +4,25 @@ import (
 	"testing"
 )
 
+type testValue struct {
+	value  int
+	actual int
+}
+
 func TestFib(t *testing.T) {
-	actual := fib(10)
-	expected := 55
-	if actual != expected {
-		t.Errorf("got %v\n want %v", actual, expected)
+	var testValues []testValue = []testValue{
+		{value: 0, actual: 0},
+		{value: 1, actual: 1},
+		{value: 2, actual: 1},
+		{value: 3, actual: 2},
+		{value: 5, actual: 5},
+		{value: 10, actual: 55},
+		{value: 20, actual: 6765},
+	}
+
+	for _, data := range testValues {
+		if fib(data.value) != data.actual {
+			t.Errorf("got %v\n want %v", fib(data.value), data.actual)
+		}
 	}
 }
